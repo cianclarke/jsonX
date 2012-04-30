345fe206ef9fb3d3a671abd4fd0df678eb1c75de
cianclarke <cian@cianclarke.com>
2012-04-30 21:52:01 +0100

    Support return type of array or map

--- routes/index.js
+++ routes/index.js
@@ -12,26 +12,34 @@ exports.index = function(req, res){
 exports.json = function(req, res){
   var body = req.body,
   limit = (body.limit && body.limit<jsonLength) ? body.limit : 50,
-  fields = body.fields || [];
+  fields = body.fields || [],
+  returnType = body.type || "array";
   
-  var data = [];
+  var data = (returnType==="array") ? [] : {};
   if (fields && fields.length>0){
-    for (var i=0; i<jsonLength; i++){
+    for (var i=0; (i<jsonLength && i<limit); i++){
       var ith = json[i];
       var obj = {};
       for (var j=0; j<fields.length; j++){
         var field = fields[j];
         if (ith.hasOwnProperty(field)){
           obj[field] = ith[field];
+        }else if(field="FullName" && ith.FirstName && ith.LastName){
+          obj['FullName'] = ith.FirstName + ' ' + ith.LastName;
         }
       }
-      data.push(obj);
+      if (returnType==="array"){
+        data.push(obj);
+      }else if(returnType==="map"){
+        data[i] = obj;
+      }
+      
     }
   }else{
     var data = json;
   }
   
-  if (limit){
+  if (limit && returnType=="array"){
     data = data.slice(0, limit);
   }
   
